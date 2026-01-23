eef19107d88bdc268c3b791213469e20ea636e90
apalala <apalala@gmail.com>
2026-01-23 14:45:25 -0400

    solutions up to 18_iterators

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -2,12 +2,15 @@
 // can offer.
 
 // TODO: Complete the `capitalize_first` function.
-// "hello" -> "Hello"
+// 1. Capitalize the first letter and append the rest of the iterator
 fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => {
+            // uppercase the first char and collect the remaining chars
+            first.to_uppercase().to_string() + chars.as_str()
+        }
     }
 }
 
@@ -15,14 +18,20 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    words.iter()
+        .map(|word| capitalize_first(word))
+        .collect()
 }
+
+
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+    words.iter()
+        .map(|word| capitalize_first(word))
+        .collect()
 }
 
 fn main() {
